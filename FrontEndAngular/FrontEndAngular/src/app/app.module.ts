2b9525bc6301e42b85984f7df2a811e8c7774297
Ceci <ceci@ceci.ce>
2022-04-05 17:50:06 -0300

    nvo componente p/separar Secc. Experiencia

--- FrontEndAngular/FrontEndAngular/src/app/app.module.ts
+++ FrontEndAngular/FrontEndAngular/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { AboutComponent } from './componentes/about/about.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HeaderComponent,
-    AboutComponent
+    AboutComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule
