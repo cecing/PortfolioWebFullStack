72f0e7bd976724746bbb56a060a211fc5819a69f
Ceci <ceci@ceci.ce>
2022-04-05 17:45:47 -0300

    componente nvo p/separar header

--- FrontEndAngular/FrontEndAngular/src/app/app.module.ts
+++ FrontEndAngular/FrontEndAngular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { HeaderComponent } from './componentes/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
