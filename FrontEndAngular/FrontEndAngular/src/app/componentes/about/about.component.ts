270adf51bba5415e537caf795c0ad76ceb3883fb
Ceci <ceci@ceci.ce>
2022-04-09 19:32:27 -0300

    corregí error en data.json

--- FrontEndAngular/FrontEndAngular/src/app/componentes/about/about.component.ts
+++ FrontEndAngular/FrontEndAngular/src/app/componentes/about/about.component.ts
@@ -11,7 +11,8 @@ export class AboutComponent implements OnInit {
   constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
-    this.datosPortfolio.obtenerDatos();
+    this.datosPortfolio.obtenerDatos().subscribe(data =>{
+      console.log(data);
+    });
   }
-
 }
