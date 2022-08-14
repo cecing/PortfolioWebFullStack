7a727fd8251f916bc68acb0c4277a1c224a067b4
Ceci <ceci@ceci.ce>
2022-08-14 12:49:16 -0300

    agregado el JWT en el Back

---
+++ BackEnd/Back/src/main/java/com/portfolio/Back/Security/Controller/Mensaje.java
@@ -0,0 +1,26 @@
+package com.portfolio.Back.Security.Controller;
+
+public class Mensaje {
+
+    private String mensaje;
+
+    //constructores
+
+    public Mensaje() {
+    }
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    //Getters&Setters
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+}
